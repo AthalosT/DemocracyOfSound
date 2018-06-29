fed48ae5a8af63755c6961ea72f598b1518b0b56
AthalosT <yijunhu1998@gmail.com>
2018-06-29 17:21:29 -0500

    generalize socketio connection

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def make_shell_context():
 
 if __name__ == '__main__':
     app.debug=True
-    port=int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    #port=int(os.environ.get("PORT", 5000))
+    socketio.run(app)
