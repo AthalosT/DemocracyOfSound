a4a14049845396e27ee7a37b9f7df3bee28bc3c9
AthalosT <yijunhu1998@gmail.com>
2018-07-05 17:45:13 -0500

    restructured survey generation to ensure a fairer distribution

--- project/voting.py
+++ project/voting.py
@@ -12,21 +12,37 @@ def generate_random_survey(sug_list_songs):
     return ret
 
 def randomize_choices(num_choices, num_shown):
-    poss_choices = set([])
+    poss_choices = []
+    for i in range(0, num_shown + 1):
+        poss_choices.append(set([]))
+
     for i in range(0, num_choices):
         choice = ChoiceRepr(i)
-        poss_choices.add(choice)
+        poss_choices[0].add(choice)
 
     choice_list = []
-    
-    while len(poss_choices) > 3:
-        random_choice = random.sample(poss_choices, 4)
-        for choice in random_choice:
-            choice_list.append(choice.id)
-            choice.times_used += 1
+    current_layer = 0
+
+    while current_layer < num_shown:
+        if len(poss_choices[current_layer]) <= 4:
+            random_choice = random.sample(poss_choices[current_layer + 1], 4 - len(poss_choices[current_layer]))
+            for choice in poss_choices[current_layer]:
+                random_choice.append(choice)
             
-            if choice.times_used >= num_shown:
-                poss_choices.remove(choice)
+            for choice in random_choice:
+                choice_list.append(choice.id)
+                poss_choices[choice.times_used].remove(choice)
+                choice.times_used += 1
+                poss_choices[choice.times_used].add(choice)
+
+            current_layer += 1
+        else:
+            random_choice = random.sample(poss_choices[current_layer], 4)
+            for choice in random_choice:
+                choice_list.append(choice.id)
+                poss_choices[choice.times_used].remove(choice)
+                choice.times_used += 1
+                poss_choices[choice.times_used].add(choice)
 
     return choice_list
 
