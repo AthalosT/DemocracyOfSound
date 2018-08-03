c3fa698cf1090e55ac8c48d3928b4876cb79f0fc
Tate Song <tlsong54321@gmail.com>
2018-08-02 19:02:36 -0500

    Change to Authorization Code Flow

--- project/spotify.py
+++ project/spotify.py
@@ -3,11 +3,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from project.lookup import cred
 
 def create_playlist(name):
-    client_credentials_manager = SpotifyClientCredentials(client_id=cred.client_id, client_secret=cred.client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    sp = spotipy.Spotify(auth='BQBZRbR2ehzPlMSEPE_faJ2h7_nXtDT66ojZbYC6Tu9jQ9l0rgBi7n62iFlvjKCAZpIDSOnTKDXS6enGqV5dS7hnOJIAgCUpcCCGrQZux6bhwa79GUcgUy8HmzsGvI_m05XAnOcvlCAOdfvBLkxgZWSpNCecv8tJE6SqjtEik_oahhVEBPlDht9DRNXM5BtgJRHoRfBd_GDYLNqp9tLytC3qgmXXK5vj9cOdUDLXOBA5')
     sp.user_playlist_create(cred.client_username, name)
 
 def add_to_playlist(playlist_id, songs):
-    client_credentials_manager = SpotifyClientCredentials(client_id=cred.client_id, client_secret=cred.client_secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    sp = spotipy.Spotify(auth='BQBZRbR2ehzPlMSEPE_faJ2h7_nXtDT66ojZbYC6Tu9jQ9l0rgBi7n62iFlvjKCAZpIDSOnTKDXS6enGqV5dS7hnOJIAgCUpcCCGrQZux6bhwa79GUcgUy8HmzsGvI_m05XAnOcvlCAOdfvBLkxgZWSpNCecv8tJE6SqjtEik_oahhVEBPlDht9DRNXM5BtgJRHoRfBd_GDYLNqp9tLytC3qgmXXK5vj9cOdUDLXOBA5')
     sp.user_playlist_add_tracks(cred.client_username, playlist_id, songs)
